76ced55ff1ab23ba009618b0a8261400359ecd33
Joze Fartek <joze.fartek@gmail.com>
2022-01-18 12:53:35 +0100

    Skale testnet migrations

--- migrations/5_deploy_nft_community.js
+++ migrations/5_deploy_nft_community.js
@@ -3,8 +3,10 @@ const EcoFiERC1155 = artifacts.require("EcoFiCommunityERC1155");
 module.exports = async function (deployer, network, accounts) {
     await deployer.deploy(EcoFiERC1155,  {overwrite: true});
     const nftC = await EcoFiERC1155.deployed();
-    await nftC.__EcoFiCommunityERC1155_init('HypE Community', 'HYPc', 'ipfs:/', 'https://stg.hyp.eco', '0xF8fdEbC27ce16013A7F1582b5812Be394cca2d76'); // Ropsten
+    //await nftC.__EcoFiCommunityERC1155_init('HypE Community', 'HYPc', 'ipfs:/', 'https://stg.hyp.eco', '0xF8fdEbC27ce16013A7F1582b5812Be394cca2d76'); // Ropsten
     //await nftC.__EcoFiCommunityERC1155_init('HypE Community', 'HYPc', 'ipfs:/', 'https://stg.hyp.eco', '0x3026c12a297DB3eab4e89fFCCd1A7818a7Ca306f'); // Rinkeby
     //await nftC.__EcoFiCommunityERC1155_init('HypE Community', 'HYPc', 'ipfs:/', 'https://hyp.eco', '0x9d523C3908Bfa05e19E124645E606977ff9B0400'); // Mainnet
+    //await nftC.__EcoFiCommunityERC1155_init('HypE Community', 'HYPc', 'ipfs:/', 'https://stg.hyp.eco', '0xcAA8ff8a8E93f1c17Cc59F70b129C49168631671'); // SKALE - beautiful-rasalgethi
+
     // name, symbol, baseURI (for file storage), contract URI (where the NFTs can be found), approved address (NFT transfer proxy)
 };
--- migrations/6_deploy_exchange.js
+++ migrations/6_deploy_exchange.js
@@ -5,26 +5,35 @@ const NftTransferProxy = artifacts.require("NftTransferProxy");
 module.exports = async function (deployer, network, accounts) {
     await deployer.deploy(EcoFiExchangeV2,  {overwrite: false});
     const excC = await EcoFiExchangeV2.deployed();
-    /* await excC.__EcoExchangeV2_init(
+    /* await excC.__EcoExchangeV2_init( // Mainnet
         '0xF8fdEbC27ce16013A7F1582b5812Be394cca2d76', // NFT transfer proxy
         '0x5651E54f9D2F8caa0Fd353a551Ded549F847A634', // ERC20 token trasfer proxy
         '50', // 0.5% exchange fee
         '0x117B9F2E941A8BF113C10554B8d7899a55dbf712', // default fee reciever
         '0x62de4fb0349dCf979b5d6A2dCac2F4340D24282e'); // royalties provider contract */
-    await excC.__EcoExchangeV2_init( // Rinkeby
+    /* await excC.__EcoExchangeV2_init( // Rinkeby
         '0x3026c12a297DB3eab4e89fFCCd1A7818a7Ca306f', // NFT transfer proxy
         '0x5871aA6C3e1fd3B7ef493285E94d860180368546', // ERC20 token trasfer proxy
         '50', // 0.5% exchange fee
         '0x117B9F2E941A8BF113C10554B8d7899a55dbf712', // default fee reciever
-        '0x152fDa837bB7A01f670DcBdcF81EDA4B1EC2d2d3'); // royalties provider contract
+        '0x152fDa837bB7A01f670DcBdcF81EDA4B1EC2d2d3'); // royalties provider contract */
 
+    await excC.__EcoExchangeV2_init( // SKALE - beautiful-rasalgethi
+        '0xcAA8ff8a8E93f1c17Cc59F70b129C49168631671', // NFT transfer proxy
+        '0x533F625e2bb3aAF5F1eb6d83e957D16653Aa6080', // ERC20 token trasfer proxy
+        '50', // 0.5% exchange fee
+        '0x117B9F2E941A8BF113C10554B8d7899a55dbf712', // default fee reciever
+        '0x347Ca24C8b22246456239fCbc27C93a002bD4523'); // royalties provider contract
     
-    //const nftTP = await NftTransferProxy.at('0xF8fdEbC27ce16013A7F1582b5812Be394cca2d76');
-    const nftTP = await NftTransferProxy.at('0x3026c12a297DB3eab4e89fFCCd1A7818a7Ca306f'); // rinkeby
+    //const nftTP = await NftTransferProxy.at('0xF8fdEbC27ce16013A7F1582b5812Be394cca2d76'); // Mainnet
+    //const nftTP = await NftTransferProxy.at('0x3026c12a297DB3eab4e89fFCCd1A7818a7Ca306f'); // Rinkeby
+    const nftTP = await NftTransferProxy.at('0xcAA8ff8a8E93f1c17Cc59F70b129C49168631671'); // SKALE - beautiful-rasalgethi
     await nftTP.addOperator(excC.address);
 
-    //const erc20TP = await ERC20TransferProxy.at('0x5651E54f9D2F8caa0Fd353a551Ded549F847A634');
-    const erc20TP = await ERC20TransferProxy.at('0x5871aA6C3e1fd3B7ef493285E94d860180368546'); // rinkeby
+    //const erc20TP = await ERC20TransferProxy.at('0x5651E54f9D2F8caa0Fd353a551Ded549F847A634'); // Mainnet
+    //const erc20TP = await ERC20TransferProxy.at('0x5871aA6C3e1fd3B7ef493285E94d860180368546'); // Rinkeby
+    const erc20TP = await ERC20TransferProxy.at('0x533F625e2bb3aAF5F1eb6d83e957D16653Aa6080'); // SKALE - beautiful-rasalgethi
+
     await erc20TP.addOperator(excC.address);
     
    
