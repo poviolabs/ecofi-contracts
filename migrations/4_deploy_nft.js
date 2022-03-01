113c81bdbc46461d29ec8037370be823cc78528d
Joze Fartek <joze.fartek@gmail.com>
2022-03-01 09:47:17 +0100

    Add Arius contracts

--- migrations/4_deploy_nft.js
+++ migrations/4_deploy_nft.js
@@ -11,6 +11,10 @@ module.exports = async function (deployer, network, accounts) {
     //await nftC.__EcoFiERC1155_init('HypE', 'HYP', 'ipfs:/', 'https://stg.hyp.eco', '0x3026c12a297DB3eab4e89fFCCd1A7818a7Ca306f'); // Rinkeby
     //await nftC.__EcoFiERC1155_init('HypE', 'HYP', 'ipfs:/', 'https://hyp.eco', '0x9d523C3908Bfa05e19E124645E606977ff9B0400'); // Mainnet
 
+    // Arius
+    //await nftC.__EcoFiERC1155_init('Arius', 'ART', 'ipfs:/', 'https://stg.arius.one', '0x3026c12a297DB3eab4e89fFCCd1A7818a7Ca306f'); // Rinkeby
+    //await nftC.__EcoFiERC1155_init('Arius', 'ART', 'ipfs:/', 'https://arius.one', '0x9d523C3908Bfa05e19E124645E606977ff9B0400'); // Mainnet
+
     // Raw
     //await nftC.__EcoFiERC1155_init('Raw', 'RAW', 'ipfs:/', 'https://stg.hyp.eco', '0x3026c12a297DB3eab4e89fFCCd1A7818a7Ca306f'); // Rinkeby
 
--- migrations/5_deploy_nft_community.js
+++ migrations/5_deploy_nft_community.js
@@ -8,5 +8,9 @@ module.exports = async function (deployer, network, accounts) {
     //await nftC.__EcoFiCommunityERC1155_init('HypE Community', 'HYPc', 'ipfs:/', 'https://hyp.eco', '0x9d523C3908Bfa05e19E124645E606977ff9B0400'); // Mainnet
     //await nftC.__EcoFiCommunityERC1155_init('HypE Community', 'HYPc', 'ipfs:/', 'https://stg.hyp.eco', '0xcAA8ff8a8E93f1c17Cc59F70b129C49168631671'); // SKALE - beautiful-rasalgethi
 
+    // Arius
+    //await nftC.__EcoFiCommunityERC1155_init('Arius Community', 'ARTc', 'ipfs:/', 'https://stg.arius.one', '0x3026c12a297DB3eab4e89fFCCd1A7818a7Ca306f'); // Rinkeby
+    //await nftC.__EcoFiCommunityERC1155_init('Arius Community', 'ARTc', 'ipfs:/', 'https://arius.one', '0x9d523C3908Bfa05e19E124645E606977ff9B0400'); // Mainnet
+
     // name, symbol, baseURI (for file storage), contract URI (where the NFTs can be found), approved address (NFT transfer proxy)
 };
